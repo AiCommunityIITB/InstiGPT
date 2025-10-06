8f590cd083ee29d4b735da6c45e1014a4edbb146
AbhishekU05 <a.upadhya05@gmail.com>
2025-10-06 15:36:22 +0530

    add logic in embeddings.py to check the config.EMBEDDING_MODEL string and initialize the correct embedding client

--- backend/instigpt/llm/embeddings.py
+++ backend/instigpt/llm/embeddings.py
@@ -16,12 +16,19 @@ def get_embeddings() -> Embeddings:
     # login to huggingface hub to access private models
     # login(os.environ["HUGGINGFACE_API_KEY"])
 
-    # embeddings = HuggingFaceEmbeddings(model_name=config.EMBEDDING_MODEL, model_kwargs = {'device': 'cpu'})
-    # embeddings = GoogleGenerativeAIEmbeddings(model=config.EMBEDDING_MODEL)  # type: ignore
 
-    embeddings = VoyageEmbeddings(
-        voyage_api_key=os.environ["VOYAGE_API_KEY"],  # type: ignore
-        model=config.EMBEDDING_MODEL,
-    )
+    # Check embedding model 
+    if config.EMBEDDING_MODEL == "voyage-large-2":
+        embeddings = VoyageEmbeddings(
+            voyage_api_key=os.environ["VOYAGE_API_KEY"],  # for voyageembeddings
+            model=config.EMBEDDING_MODEL,
+        )
+    elif config.EMBEDDING_MODEL == "jinaai/jina-embeddings-v2-base-en": # for huggingfaceembeddings
+        embeddings = HuggingFaceEmbeddings(model_name=config.EMBEDDING_MODEL,
+        model_kwargs = {'device': 'cpu'})
+    elif config.EMBEDDING_MODEL == "models/embedding-001": # For google gen ai
+        embeddings = GoogleGenerativeAIEmbeddings(model=config.EMBEDDING_MODEL)  
+    else:
+        raise Exception("config.EMBEDDING_MODEL not set")
 
     return embeddings
